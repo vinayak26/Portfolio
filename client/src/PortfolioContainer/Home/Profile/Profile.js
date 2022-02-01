718a9075093e396205b0b881d654d33d1712808b
Vinayak <Vortex141>
2022-02-01 16:04:30 +0530

    FINAL

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Typical from "react-typical";
+import ScrollService from "../../../utilities/ScrollService";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://github.com/Vort3x141">
+                <i className="fa fa-github" />
+              </a>
+              <a href="https://linkedin.com/in/vinayakmishra26">
+                <i className="fa fa-linkedin-square" />
+              </a>
+              <a href="mailto:vinayakmishra26@gmail.com">
+                <i className="fa fa-envelope" />
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Vinayak</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 💻",
+                    1000,
+                    "Learning Mern stack 📱",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="profile-role-tagline">
+              Knack of building applications with front and back end operations.
+            </span>
+          </div>
+
+          <div className="profile-options">
+            <button className="btn primary-btn"
+            onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            > Hire Me </button>
+            <a href="VinayakMishraResume.pdf" download="VinayakMishraResume.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
